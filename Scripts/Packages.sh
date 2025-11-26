96073c6e98b68e6ffb5ecb523266cb70eacc4503
谭新棋 <652605571@qq.com>
2025-11-26 11:38:25 +0800

    增加adguardHome

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -73,6 +73,9 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
+UPDATE_PACKAGE "luci-app-adguardhome" "kenzok8/openwrt-packages" "main"
+UPDATE_PACKAGE "adguardhome" "kenzok8/openwrt-packages" "main"
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
